c6bdb5c7921c82cda9f9dedda8a769b57ab81f34
Haidir <Haidir@DESKTOP-PO136QB>
2024-06-06 16:14:39 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DreamRestBeds.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,14 @@
+﻿Team - Epsilon
+Members:
+Aditya, Haidir, Akhil
+
+2024/06/06
+
+1601
+Created Company name. The name is DreamRest Beds
+
+1606
+Created company logo.
+
+1610
+Created MVC aplication.
